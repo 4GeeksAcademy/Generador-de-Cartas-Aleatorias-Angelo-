6201b1c2c50cb1d2a4b843822505272bae4663af
zebhastyanangelo-svg <zebhastyanangelo@gmail.com>
2026-04-28 00:01:07 +0000

    se crearon 1 arreglo,2funciones y 1 variable

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 window.onload = function() {
   
     let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let suit = ["Diamonds", "Spades", "Hearts", "Clubs"];
+    let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+    let randomSuitNumber = Math.floor(Math.random() * suit.length);
+    let finalSuit = suit[randomSuitNumber];
+    
     
   
